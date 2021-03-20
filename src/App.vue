e98584ca827f4fa2bf3bd9b3da35ad177ebd7433
Austinchambers1 <austinchambers1@gmail.com>
2021-03-19 21:24:48 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container">
+    <LandingPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>

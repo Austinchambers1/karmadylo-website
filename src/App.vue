3bcbf2432ba281bde03d9f607186410368c84a7e
Austinchambers1 <austinchambers1@gmail.com>
2021-03-22 00:45:46 -0500

    tweaking image, making socials less obnoxiously spaced out

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-  <div id="app" class="container">
-    <LandingPage msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <LandingPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import LandingPage from './components/LandingPage.vue'
+import LandingPage from "./components/LandingPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    LandingPage
-  }
-}
+    LandingPage,
+  },
+};
 </script>
 
 <style>
+body,
+html {
+  height: 100%;
+}
+.parallax {
+  background-image: url("./assets/karmadylo.jpg");
+  /* Full height */
+  height: 100%;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 #app {
   text-align: center;
+  height: 3000px;
+  background-color: #d7c5eb;
 }
 </style>
